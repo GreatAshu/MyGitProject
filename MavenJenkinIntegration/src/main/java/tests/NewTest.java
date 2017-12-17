1c65053c6b8fc5df986b7762f57bdb366d4c48a7
ashua <ashua@wsltashua.starhome.local>
2017-12-17 16:58:36 +0530

    First Test Commit

---
+++ MavenJenkinIntegration/src/main/java/tests/NewTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	public WebDriver driver;
+	
+	  @Test
+	  public void openMyBlog() {
+		driver.get("http://www.softwaretestingmaterial.com/");
+	  }
+	  
+	  @BeforeClass
+	  public void beforeClass() {
+		  
+		  System.setProperty("webdriver.chrome.driver", "D:\\WorkSpaceSHM\\Miscellaneous Projects\\2_33_Chrome\\chromedriver.exe");
+		  driver = new ChromeDriver();
+		  
+	  }
+
+	  @AfterClass
+	  public void afterClass() {
+		  driver.quit();
+	  }
+
+}
